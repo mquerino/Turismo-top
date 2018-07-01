2388874c811ebde2161ac11c3964bb88fc0f8c80
Marcos Fidelis Querino <38235474+mquerino@users.noreply.github.com>
2018-07-01 16:54:22 -0300

    upload
    cidades

---
+++ armazem.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <title>ADM Guia Turístico</title>
+        <link rel="stylesheet" href="../estilo.css">
+
+</head>
+<body>
+  <fieldset>
+
+<div class="flex-container">
+<header>
+  <h1 id="prin"><img src="../imagens/logo.jpeg" width="24" height="24">ADM Guia Turístico</h1>
+</header>
+<fieldset>
+<nav class="nav">
+        <fieldset><center>
+                <ul>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="../administrador.php">Administrador</a></li>
+                    <li><a href="../formulario.php">Enviar uma mensagem</a></li>
+                </ul>
+                <ul>
+                    <li><a href="armazem.html">Armazém</a></li>
+  <li><a href="braco.php">Braço do Norte</a></li>
+  <li><a href="capiva.php">Capivari de Baixo</a></li>
+  <li><a href="grao.php">Grão Pará</a></li>
+  <li><a href="gravatal.php">Gravatal</a></li>
+  <li><a href="imarui.php">Imaruí</a></li>
+  <li><a href="imbituba.php">Imbituba</a></li>
+  <li><a href="jaguaruno.php">Jaguaruna</a></li>
+  <li><a href="laguna.php">Laguna</a></li>
+  <li><a href="pedras.php">Pedras Grandes</a></li>
+  <li><a href="pesca.php">Pescaria Brava</a></li>
+  <li><a href="rio.php">Rio Fortuna</a></li>
+  <li><a href="sangao.php">Sangão</a></li>
+  <li><a href="santa.php">Santa Rosa de Lima</a></li>
+  <li><a href="sao.php">São Ludgero</a></li>
+  <li><a href="saomar.php">São Martinho</a></li>
+  <li><a href="treze.php">Treze de Maio</a></li>
+  <li><a href="tubarao.php">Tubarão</a></li>
+                </ul>        
+            </center></fieldset>
+</nav>
+</fieldset>
+<h1><strong>ARMAZÉM</strong></h1>
+<fieldset>
+        <img src="../imagens/Bandeira_de_Armazém.jpg">
+        <h2>Origens e povoamento</h2>
+        <p>Em Santa Catarina, os imigrantes que vieram dos Açores colonizaram, em primeiro lugar, as regiões localizadas 
+          no litoral, (o fato, é salientado, tendo validade para a totalidade do Brasil), e, em segundo lugar, o planalto.
+          Pórtico de entrada de Armazém
+          No momento em que nessas regiões eram estabelecidos os vicentistas e açorianos, eram fixados no planalto, durante 
+          uma grande quantidade de anos após, paulistas de Piratininga e de demais regiões. Havia dificuldade e lentidão 
+          entre ambas as áreas intercambiadas, por causa das serras do Mar e Geral, muralhadas, que se interpõem entre as 
+          duas. O primeiro caminho, era feito da região de Laguna até os Campos de Lages. Era a Estrada dos Conventos. Por 
+          ali ocorria o trânsito dos tropeiros, que desciam da "Serra", com produtos pastoris e sendo os tropeiros, 
+          propriamente ditos, condutores, no caminho de volta, de artigos de primeira necessidade (sal, tecidos, querosene, 
+          etc).
+          O município passou a chamar-se Armazém, porque ali existia uma casa comercial de menor porte que se estabeleceu
+           nos "Sertões dos Corrêas" (os Corrêas seriam os primeiros habitantes de Armazém), ponto de frequência dos 
+           "Serranos".</p>
+        <p>A história do município, é prendida, de modo íntimo, à de Tubarão, da qual era distrito em 1901. De acordo 
+          com a Lei nº 380, de 19 de dezembro de 1958, era criando o município de Armazém, com território que se
+          desmembrou de Tubarão. O novo município foi instalado em 28 de janeiro de 1959.[6]
+          O prefeito escolhido sob nomeação do governo estadual foi o senhor José Diomário da Rosa e o senhor Paulo Wensing, 
+          o prefeito que venceu as primeiras eleições municipais</p>
+        <p>Armazém está a 30 km de Tubarão e da BR-101. Para chegar, vindo do sul ou do norte do estado, siga pela BR 101 e 
+          tome direção oeste pela SC-438 hoje SC 370 até Gravatal, seguindo então pela SC-431 hoje SC 435. Para quem vem do 
+          oeste, o melhor caminho é a rota Lages - São Joaquim - Gravatal, seguindo pela mesma SC-438 atual SC 370.</p>
+
+      </article>
+      </fieldset>
+      <fieldset>
+          <anside class="anside">  
+              <h3><a target="_blank" href="https://www.google.com.br/maps/place/Armaz%C3%A9m+-+SC/@-28.2339741,-49.0728031,12z/data=!3m1!4b1
+                !4m5!3m4!1s0x95213d0197bde885:0xedac0d70433b846d!8m2!3d-28.2630149!4d-49.0198837">Localização</h3></a>    
+          </anside>
+          </fieldset>
+      
+      <center><h4><a href="#prin"><img src="../imagens/Topo.jpg" width="50" height="50"></a> </h4></center>
+      <footer>Copyright &copy; adm.com.br</footer>
+      </div>
+      </fieldset>
+      </body>
+      </html>
+      
---
+++ imarui.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <title>ADM Guia Turístico</title>
+        <link rel="stylesheet" href="../estilo.css">
+
+</head>
+<body>
+  <fieldset>
+
+<div class="flex-container">
+<header>
+  <h1 id="prin"><img src="imagens/logo.jpeg" width="24" height="24">ADM Guia Turístico</h1>
+</header>
+<fieldset>
+<nav class="nav">
+        <fieldset><center>
+                <ul>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="../administrador.php">Administrador</a></li>
+                    <li><a href="../formulario.php">Enviar uma mensagem</a></li>
+                </ul>
+                <ul>
+                    <li><a href="armazem.php">Armazém</a></li>
+  <li><a href="braco.php">Braço do Norte</a></li>
+  <li><a href="capiva.php">Capivari de Baixo</a></li>
+  <li><a href="grao.php">Grão Pará</a></li>
+  <li><a href="gravatal.php">Gravatal</a></li>
+  <li><a href="imarui.php">Imaruí</a></li>
+  <li><a href="imbituba.php">Imbituba</a></li>
+  <li><a href="jaguaruno.php">Jaguaruna</a></li>
+  <li><a href="laguna.php">Laguna</a></li>
+  <li><a href="pedras.php">Pedras Grandes</a></li>
+  <li><a href="pesca.php">Pescaria Brava</a></li>
+  <li><a href="rio.php">Rio Fortuna</a></li>
+  <li><a href="sangao.php">Sangão</a></li>
+  <li><a href="santa.php">Santa Rosa de Lima</a></li>
+  <li><a href="sao.php">São Ludgero</a></li>
+  <li><a href="saomar.php">São Martinho</a></li>
+  <li><a href="treze.php">Treze de Maio</a></li>
+  <li><a href="tubarao.php">Tubarão</a></li>
+                </ul>        
+            </center></fieldset>
+</nav>
+</fieldset>
+<h1><strong>IMARUÍ</strong></h1>
+<fieldset>
+        <img src="https://pt.wikipedia.org/wiki/Ficheiro:Catedral_tubar%C3%A3o.jpg">
+        
+        <p>Tubarão é um município brasileiro localizado no sul do estado de Santa Catarina. 
+          Situa-se na latitude 28º28'00" sul e longitude 49º00'25" oeste, estando a uma altitude 
+          média de 9 metros acima do nível do mar, com uma área de 301,755 km². De acordo com o 
+          IBGE a população total observada no último recenseamento realizado em 2010 foi de 97.235 
+          habitantes. Conforme estimativa realizada pelo IBGE, em 2017 o município possuía 104.457 
+          moradores.</p>
+        <p>O nome da cidade deve-se ao rio Tubarão, que em tupi-guarani era chamado Tubá-Nharô,[6] 
+          "pai feroz". Outra versão corrente relaciona ainda este nome com o de um cacique muito 
+          influente que habitava a região. Não se relaciona, porém, ao peixe homônimo.</p>
+        <p><strong>Com a abertura do caminho entre Lages e Tubarão, por volta de 1773, iniciou-se o 
+          povoamento da cidade. O rio Tubarão era parte da rota Lages/Laguna, tendo como ponto de 
+          parada os portos do "Poço Fundo" e do "Poço Grande", ambos na região da atual Tubarão. 
+          Em agosto de 1774, duas sesmarias, com área de uma légua quadrada[nota 1] cada uma, situadas
+           no atual perímetro urbano, foram doadas ao capitão João da Costa Moreira e ao sargento-mor 
+           Jacinto Jaques Nicós,[7][8] marcando o início efetivo do povoamento.</strong></p>
+
+      </article>
+      </fieldset>
+      <fieldset>
+          <anside class="anside">  
+              <h3><a target="_blank" href="https://www.google.com.br/maps/place/Imaru%C3%AD+-+SC/@-28.215908,-48.9815903,
+                11z/data=!3m1!4b1!4m5!3m4!1s0x9521496bad059165:0x8fa295e9d3d7d909!8m2!3d-28.3437512!4d-48.
+                8164937">Localização</h3></a>
+          </anside>
+      </fieldset>
+      
+      <fieldset>
+      <center><h4><a href="#prin"><img src="../imagens/Topo.jpg" width="50" height="50"></a> </h4></center>
+      <footer>Copyright &copy; adm.com.br</footer>
+      </div>
+      </fieldset>
+  </body>
+</html>
+      
